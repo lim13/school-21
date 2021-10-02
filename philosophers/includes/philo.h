bbef5ac757c70f66edac3b37329fda05eb13a8a5
Clarinda Dwyynen <cdwyynen@at-k7.msk.21-school.ru>
2021-10-02 13:11:27 +0300

    philo done

--- philosophers/includes/philo.h
+++ philosophers/includes/philo.h
@@ -1,12 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdwyynen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/02 11:27:25 by cdwyynen          #+#    #+#             */
+/*   Updated: 2021/10/02 11:27:27 by cdwyynen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef PHILO_H
-#define PHILO_H
+# define PHILO_H
 
-#include <unistd.h>
-#include <stdio.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
 
+typedef struct s_data
+{
+	int				number_of_philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				number_of_times_each_philosopher_must_eat;
+	int				dead;
+	int				everyone_is_full;
+	struct timeval	start;
+	pthread_mutex_t	forks[250];
+	pthread_mutex_t	mutex_food;
+	pthread_mutex_t	mutex_print;
+}	t_data;
 
+typedef struct s_philosopher
+{
+	int				index;
+	int				right_fork;
+	int				left_fork;
+	struct timeval	last_time_eating;
+	int				number_of_times_ate;
+	pthread_t		philosopher_thread;
+	t_data			*philosopher_dataset;
+}	t_philosopher;
 
-
+int		ft_atoi(const char *str);
+int		ft_philosophers_threads(t_data *data, t_philosopher *philo);
+void	ft_waiter(t_data *data, t_philosopher *philo);
 
 #endif
---
+++ philosophers/srcs/ft_atoi.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdwyynen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/02 11:27:13 by cdwyynen          #+#    #+#             */
+/*   Updated: 2021/10/02 11:27:15 by cdwyynen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	n;
+	int	sign;
+
+	sign = 1;
+	n = 0;
+	i = 0;
+	if ((str[i] <= 47 || str[i] >= 58) && str[i] != '\t' && str[i] != '\n'
+		&& str[i] != '\v' && str[i] != '\f' && str[i] != '\r' && str[i] != ' '
+		&& str[i] != 45 && str[i] != 43)
+		return (0);
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || str[i] == '\f' \
+	|| str[i] == '\r' || str[i] == ' ')
+		i = i + 1;
+	if (str[i] == 45)
+	{
+		sign = -1 ;
+		i = i + 1;
+	}
+	else if (str[i] == 43)
+		i = i + 1;
+	while ((str[i] != '\0') && (str[i] >= 48 && str [i] <= 57))
+		n = n * 10 + (str[i++] - '0');
+	return (sign * n);
+}
